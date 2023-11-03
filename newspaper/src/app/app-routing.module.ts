f6294d9ac279485caff94dd0be018c9e7a4c5b70
unknown <aliibneimran@gmail.com>
2023-11-03 09:14:22 +0600

    update

--- newspaper/src/app/app-routing.module.ts
+++ newspaper/src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { WorldComponent } from './world/world.component';
+import { TravelComponent } from './travel/travel.component';
+import { TechComponent } from './tech/tech.component';
+import { FashionComponent } from './fashion/fashion.component';
+import { SportComponent } from './sport/sport.component';
+import { HealthComponent } from './health/health.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'world', component: WorldComponent},
+  { path: 'travel', component: TravelComponent},
+  { path: 'tech', component: TechComponent},
+  { path: 'fashion', component: FashionComponent},
+  { path: 'sport', component: SportComponent},
+  { path: 'health', component: HealthComponent},
 ];
 
 @NgModule({
--- newspaper/src/app/app.module.ts
+++ newspaper/src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { WorldComponent } from './world/world.component';
+import { TravelComponent } from './travel/travel.component';
+import { TechComponent } from './tech/tech.component';
+import { FashionComponent } from './fashion/fashion.component';
+import { SportComponent } from './sport/sport.component';
+import { HealthComponent } from './health/health.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    WorldComponent,
+    TravelComponent,
+    TechComponent,
+    FashionComponent,
+    SportComponent,
+    HealthComponent
   ],
   imports: [
     BrowserModule,

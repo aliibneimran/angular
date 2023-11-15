7b63644eb61979aa7d865859ecc4a2ab25ae0242
unknown <aliibneimran@gmail.com>
2023-11-15 22:37:22 +0600

    add

--- angularnews/src/app/post.service.ts
+++ angularnews/src/app/post.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { map } from 'rxjs/operators';
-import { Post } from './post';
+// import { Post } from './post';
 
 @Injectable({
   
   providedIn: 'root'
 })
 export class PostService {
-  baseUrl = 'http://localhost/wdpf55_angular/newspaper/api';
+  baseUrl = 'http://localhost/wdpf55_angular/angularnews/api';
   constructor(private http: HttpClient) { }
 
                   
